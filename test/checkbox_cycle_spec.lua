216cda6b9bebbca8e6216a2e21c75de2bc6f21f6
Emmanuel Pilande <epilande@gmail.com>
2024-09-10 00:29:14 -0700

    chore: Add tests for multiple state cycles in checkbox
    This commit adds tests for multiple state cycles in the checkbox
    cycle functionality. It verifies that the checkbox cycle can handle
    multiple states and that the correct state is used when a cycle
    index is specified. This ensures the robustness of the checkbox
    cycle feature.

--- test/checkbox_cycle_spec.lua
+++ test/checkbox_cycle_spec.lua
@@ -59,4 +59,41 @@ describe('checkbox-cycle', function()
     checkbox_cycle.cycle_next()
     assert.are.same({ '  - [ ] Indented checkbox' }, vim.api.nvim_buf_get_lines(0, 0, -1, false))
   end)
+
+  it('supports multiple state cycles', function()
+    checkbox_cycle.setup({
+      states = {
+        { '- [ ]', '- [x]', '- [?]' },
+        { '- [!]', '- [~]', '- [-]' },
+      },
+    })
+
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { '- [ ] First cycle item' })
+    checkbox_cycle.cycle_next()
+    assert.are.same({ '- [x] First cycle item' }, vim.api.nvim_buf_get_lines(0, 0, -1, false))
+
+    vim.api.nvim_buf_set_lines(0, 1, -1, false, { '- [!] Second cycle item' })
+    vim.api.nvim_win_set_cursor(0, { 2, 0 })
+    checkbox_cycle.cycle_next()
+    assert.are.same(
+      { '- [x] First cycle item', '- [~] Second cycle item' },
+      vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    )
+  end)
+
+  it('uses the specified state cycle when cycling with cycle_index', function()
+    checkbox_cycle.setup({
+      states = {
+        { '- [ ]', '- [x]', '- [?]' },
+        { '- [>]', '- [<]' },
+        { '- [!]', '- [~]', '- [-]' },
+      },
+    })
+
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { 'No checkbox here' })
+    checkbox_cycle.cycle_next(3)
+    assert.are.same({ '- [!] No checkbox here' }, vim.api.nvim_buf_get_lines(0, 0, -1, false))
+    checkbox_cycle.cycle_next(3)
+    assert.are.same({ '- [~] No checkbox here' }, vim.api.nvim_buf_get_lines(0, 0, -1, false))
+  end)
 end)
